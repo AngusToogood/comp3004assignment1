bf07b9dddcbc865d20bc51425ad4f4674d2a95a2
TheClemening <toogoodangus@gmail.com>
2018-09-18 11:38:41 -0400

    Test: basic hand functionality 	- 2 types of hands 	- cards can be added to hands 	- hands can be displayed with score 	- dealer hands are hidden when not their turn 	- no ace scoring or playing functionality yet

---
+++ src/test/java/comp3004assignment1/HandTest.java
@@ -0,0 +1,22 @@
+package comp3004assignment1;
+
+import junit.framework.TestCase;
+
+public class HandTest extends TestCase {
+	public void testDraw() {
+		PlayerHand p = new PlayerHand();
+		DealerHand d = new DealerHand();
+		Deck deck = BlackJack.fileToDeck("src/test/resources/fileReadTest.txt");
+		p.add(deck.draw());
+		p.add(deck.draw());
+		assertEquals(2, p.size());
+		assertEquals("Player: C3 HK (13)", p.toString());
+		d.add(deck.draw());
+		d.add(deck.draw());
+		assertEquals(2, d.size());
+		d.turn = false;
+		assertEquals("Dealer: S2 **", d.toString());
+		d.turn = true;
+		assertEquals("Dealer: S2 D7 (9)", d.toString());
+	}
+}
